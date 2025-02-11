ee541b21fe69c9b98521d77508c9e28e95ee1568
Hermayonick-Catalina-Hernandez-Gonzalez <2130036@upv.edu.mx>
2025-02-11 11:27:25 -0600

    Cambios

--- vistas/historial.php
+++ vistas/historial.php
@@ -24,6 +24,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Historial Vehicular</title>
+    <link rel="shortcut icon" href="../img/Icono.png" />
     <link rel="stylesheet" href="../css/styleshistorial.css">
 </head>
 <body>
--- vistas/inicio.php
+++ vistas/inicio.php
@@ -12,6 +12,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verificación Vehicular</title>
+    <link rel="shortcut icon" href="../img/Icono.png" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/stylesInicio.css">
 </head>
